9516c6b6ce26a5fe2cc065bb32af6f6fb2bc5f90
ethelhenrard <ethelhenrard@hotmail.com>
2020-05-12 15:00:59 +0200

    Upload de l'image > creation d'un service

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Recipe;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +21,7 @@ class RecipeController extends AbstractController
     /**
      * @Route("/", name="recipe_index", methods={"GET"})
      */
+
     public function index(RecipeRepository $recipeRepository): Response
     {
         return $this->render('recipe/index.html.twig', [
@@ -29,13 +32,22 @@ class RecipeController extends AbstractController
     /**
      * @Route("/new", name="recipe_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    // injection de dependance/service en rajoutant parametre FileUploader
+    public function new(Request $request, FileUploader $fileUploader): Response
     {
         $recipe = new Recipe();
         $form = $this->createForm(RecipeType::class, $recipe);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $pictureFile */
+            $pictureFile = $form['pictureFile']->getData();
+
+            if ($pictureFile) {
+                $pictureFilename = $fileUploader->upload($pictureFile);
+                $recipe->setPicture($pictureFilename);
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($recipe);
             $entityManager->flush();
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -9,6 +9,7 @@ use App\Entity\Tag;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,7 +28,10 @@ class RecipeType extends AbstractType
             ->add('bakingTime')
             ->add('preparationTime')
             ->add('nbPersonne')
-            ->add('picture')
+            ->add('pictureFile', FileType::class, [
+                //oui je sais que pictureFile n'existe pas dans l'entité dc pas mappé. on fera le lien dans notre code
+                'mapped' => false
+            ])
             ->add('difficulty')
             ->add('category', EntityType::class,[
         'class' => Category::class,
